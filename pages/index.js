0983a8e40604e631334802892f93b70724223c1a
daniel.igor.rodrigues <daniel.igor.rodrigues@gmail.com>
2024-10-02 00:02:44 -0300

    ajuste da página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <h1>Tuane, eu amo você, se você me ama também, me manda um coração!</h1>
+    <h1>Página em desenvolvimento! Esta página tem o propósito de armazenar minhas ideias e projetos, tem como objetivo ser o "ponta pé" que falta para startar a minha carreira como dev.</h1>
   );
 }
 
